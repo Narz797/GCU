74852d2410a8314ddbb57975a940d4f2d4095f30
Narz <101245624+Narz797@users.noreply.github.com>
2023-06-28 08:33:31 +0800

    smaller banner

--- Student_Side/includes/banner.php
+++ Student_Side/includes/banner.php
@@ -1,6 +1,6 @@
-<div style="background-image: url('assets/img/bg.png'); background-repeat: no-repeat; background-size: cover; padding-bottom: 50px;">
+<div style="background-image: url('assets/img/bg.png'); background-repeat: no-repeat; background-size: cover; display:flex; justify-content:center; padding-top:2%; padding-bottom:2%;">
   <br>
-  <img src="assets/img/GCU.png" alt="" style="display: inline-block; vertical-align: middle; margin-left: 21%; width:14%; height:auto;">
+  <img src="assets/img/GCU.png" alt="" style="display: inline-block; vertical-align: middle;  width:10%; height:auto;">
   <div id="RBG" style="display: inline-block; vertical-align: middle;">
     <h5 style="font-family: 'Georgia', serif;">REPUBLIC OF THE PHILIPPINES</h5>
     <hr class="line" style="width: 100%; border-color: black; margin-bottom: 0;">
@@ -12,12 +12,12 @@
 <style>
 
 #RBG h5{
-  font-size: 1.4vw;
+  font-size: 1vw;
  }
  #RBG h3{
-  font-size: 2vw;
+  font-size: 1.8vw;
  }
  #RBG h1{
-  font-size: 2.8vw;
+  font-size: 2vw;
  }
  </style>
