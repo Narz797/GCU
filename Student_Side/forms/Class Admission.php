1f1963e364a3ab586db8385ef8eb8c469087231c
JulianaMae2002 <cianreviere@gmail.com>
2023-07-01 16:51:22 +0800

    made edits on the design

--- Student_Side/forms/Class Admission.php
+++ Student_Side/forms/Class Admission.php
@@ -2,14 +2,33 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Class Admission slip</title>
+<title>Class Admission Slip</title>
 <style type="text/css">
 	#Title{
 		text-align: center;
+    font-family: Arial, sans-serif;
 	}
 	.hidden1, .hidden2{
 		display: none;
 	}
+  body{
+    background-color:#E8E4C9;
+    
+  }
+  .form {
+      width: 90%;
+      padding: 60px;
+      padding-right:60px;
+      background-color: cream;
+      border-radius: 8px;
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+      
+    }
+  .form label{
+    color:black;
+    font-size:20px;
+    font-family: Arial, sans-serif;
+  }
 	
 </style>
 	
@@ -17,6 +36,7 @@
 
 <body>
 <h1 id="Title">Class Admission Slip</h1>
+<div class="form">
 <form id="form1" name="form1" method="post">
   <p>
     <label for="number">Student ID No.:</label>
@@ -68,10 +88,13 @@
 		<label for="textfield4">official co/extra-curricular activity:</label>
 		<input type="text" name="textfield4" id="textfield4">
 	</div>
+  <div class="button">
   <p>
     <input type="submit" name="submit" id="submit" value="Submit">
   </p>
+  </div>
 </form>
+</div>
 	<script>
 		const dropdown = document.getElementById('coa');
 		const textfield1 = document.getElementById('others');
