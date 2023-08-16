b9c0faa8da13c698feeff0ff08735501ec0ab790
Narz <101245624+Narz797@users.noreply.github.com>
2023-08-17 06:34:54 +0800

    Update db_gcu.sql

--- database/db_gcu.sql
+++ database/db_gcu.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 25, 2023 at 12:51 PM
+-- Generation Time: Aug 17, 2023 at 12:30 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `db_gcu`
 --
+CREATE DATABASE IF NOT EXISTS `db_gcu` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `db_gcu`;
 
 -- --------------------------------------------------------
 
@@ -27,8 +29,9 @@ SET time_zone = "+00:00";
 -- Table structure for table `admin_user`
 --
 
-CREATE TABLE `admin_user` (
-  `user_id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `admin_user`;
+CREATE TABLE IF NOT EXISTS `admin_user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `admin_user_id` int(11) NOT NULL,
   `first_ name` text NOT NULL,
   `last_name` text NOT NULL,
@@ -37,15 +40,16 @@ CREATE TABLE `admin_user` (
   `position` text NOT NULL,
   `email` text NOT NULL,
   `username` text NOT NULL,
-  `password` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `password` text NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `admin_user`
 --
 
 INSERT INTO `admin_user` (`user_id`, `admin_user_id`, `first_ name`, `last_name`, `middle_name`, `gender`, `position`, `email`, `username`, `password`) VALUES
-(1, 2002129, 'Narz Josef', 'Taquio', 'Ligawad', 'Male', 'Admin', 'narz@gmail.com', 'Narz', 'password');
+(15, 2002529, 'Narz Josef', 'Taquio', 'L.', 'Male', 'admin', 'josefnarz2011@gmail.com', 'Narz', '$2y$10$vrzb0bq0qR6EeXUQrWzOielCpmRrMiY246zbJrJz/H01go267FBCW');
 
 -- --------------------------------------------------------
 
@@ -53,8 +57,9 @@ INSERT INTO `admin_user` (`user_id`, `admin_user_id`, `first_ name`, `last_name`
 -- Table structure for table `student_user`
 --
 
-CREATE TABLE `student_user` (
-  `user_id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `student_user`;
+CREATE TABLE IF NOT EXISTS `student_user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `stud_user_id` int(11) DEFAULT NULL,
   `first_name` text DEFAULT NULL,
   `last_name` text DEFAULT NULL,
@@ -65,47 +70,25 @@ CREATE TABLE `student_user` (
   `birth_date` date DEFAULT NULL,
   `email` text DEFAULT NULL,
   `username` text DEFAULT NULL,
-  `password` text DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `password` text DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `student_user`
 --
 
 INSERT INTO `student_user` (`user_id`, `stud_user_id`, `first_name`, `last_name`, `middle_name`, `gender`, `year_enrolled`, `course`, `birth_date`, `email`, `username`, `password`) VALUES
-(1, 2001518, 'Jonray', 'Tacudog', 'Bernard', 'Male', NULL, NULL, NULL, 'tacudog.jonray@gmail.com', 'spellarj', '12345');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `admin_user`
---
-ALTER TABLE `admin_user`
-  ADD PRIMARY KEY (`user_id`);
-
---
--- Indexes for table `student_user`
---
-ALTER TABLE `student_user`
-  ADD PRIMARY KEY (`user_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `admin_user`
---
-ALTER TABLE `admin_user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `student_user`
---
-ALTER TABLE `student_user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+(1, 2001518, 'Jonray', 'Tacudog', 'Bernard', 'Male', NULL, NULL, NULL, 'tacudog.jonray@gmail.com', 'spellarj', '12345'),
+(4, 6543, 'test', 'test', 't', 'Male', 2005, 'BSIT', '2023-08-23', 'narz@gmail.com', 'student2', 'pass2'),
+(16, 432, 'Narz Josef', 'Taquio', 'L.', 'Male', 2020, 'BSIT', '0000-00-00', 'josefnarz2011@gmail.com', 'Narz', '$2y$10$Qsu.qwvkZ1ProVhvKXzCceopW3769HusjdHNfgzrouJH43BH0jISy'),
+(17, 111, 'John', 'Doe', 'D', 'Male', 2020, 'BLIS', '2023-08-15', 'user2@gmail.com', 'user2', '$2y$10$SczVZE3o64M8ZhuB1.NtG.AEY38qQFypzZ/FCW2m1sHyIR0eVEeSO'),
+(18, 112, 'Jane', 'D.', 'Doe', 'Female', 2019, 'BSDC', '2023-08-21', 'user3@gmail.com', 'user3', '$2y$10$jU4Y./C62dj97yonb5Wk7u02Spu3cnzv5sjNJXBj.8U1M77T7ScOy'),
+(19, 114, 'Josh', 'Kun', 'S', 'Male', 2020, 'BSIT', '2023-08-11', 'user4@gmail.com', 'user4', '$2y$10$4c3P3QQ2hUkIu8hffGl0ZewQB3ZHzfu1uNV76W9fiy/A/IhELhD5q'),
+(20, 115, 'Shinomiya', 'Kaguya', 'S', 'Female', 2019, 'BSDC', '2023-08-11', 'user5@gmail.com', 'user4', '$2y$10$Bc5xD1fwgVaDhyfodnoRpeWEqq3wys7oO1rEXODkS6bLBFxwxkPH.'),
+(21, 116, 'Miyuki', 'Shirogane', 'S.', 'Male', 2019, 'BSIT', '2023-08-18', 'user6@gmail.com', 'user6', '$2y$10$ijDxvfz1Nwj/W71jek9d1Oi/E3glb8U/HAqoCq9vqCHPwp3/BraT2'),
+(22, 117, 'Luffy', 'Monkey', 'D', 'Male', 2020, 'BLIS', '2023-08-18', 'user7@gmail.com', 'user7', '$2y$10$tu1lLwa.V8I728aNkNnpbuwdBckqpv46jpwAPJpVkwDDdPvqIQYmC'),
+(23, 118, 'test8', 'test', 't', 'Female', 2020, 'qwer', '2023-08-17', 'user8@gmail.com', 'user8', '$2y$10$RKW1IMlG82XCHHjAevPL0e.YPsPzqFv6ibl70MIvXTheK90sQtf0a');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
