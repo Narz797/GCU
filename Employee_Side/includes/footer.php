dabf406189b89587f2770fec386adfd5470514bc
Narz <101245624+Narz797@users.noreply.github.com>
2023-08-22 12:14:00 +0800

    organized (Employee side)

---
+++ Employee_Side/includes/footer.php
@@ -0,0 +1,70 @@
+<footer id="footer" class="footer">
+
+    <div class="container">
+      <div class="row gy-4">
+        <div class="col-lg-5 col-md-12 footer-info" id="titlefooter">
+          <div title="footertitle">
+          <a href="index.php" class="logo d-flex align-items-center">
+            <span>Guidance and Counseling Unit</span>
+          </a>
+          </div>
+          <p>Benguet State University</p>
+          <div class="social-links d-flex mt-4">
+            <a href="#" class="twitter"><i class="ri-twitter-line"></i></a>
+            <a href="#" class="facebook"><i class="ri-facebook-fill"></i></a>
+            <a href="#" class="instagram"><i class="ri-instagram-line"></i></a>
+            <a href="#" class="linkedin"><i class="ri-linkedin-fill"></i></a>
+          </div>
+        </div>
+
+        <div class="col-lg-2 col-6 footer-links" id="footerlinks">
+          <h4>Useful Links</h4>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About us</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Terms of service</a></li>
+            <li><a href="#">Privacy policy</a></li>
+          </ul>
+        </div>
+
+        <div class="col-lg-2 col-6 footer-links" id="footerservices">
+          <h4>Our Services</h4>
+          <ul>
+            <li><a href="#">Forms</a></li>
+            <li><a href="#">Forms</a></li>
+            <li><a href="#">Forms</a></li>
+            <li><a href="#">Forms</a></li>
+            <li><a href="#">Forms</a></li>
+          </ul>
+        </div>
+
+        <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start" id="footercontacts">
+          <h4>Contact Us</h4>
+          <p>
+            A108 Adam Street <br>
+            New York, NY 535022<br>
+            United States <br><br>
+            <strong>Phone:</strong> +1 5589 55488 55<br>
+            <strong>Email:</strong> info@example.com<br>
+          </p>
+
+        </div>
+
+      </div>
+    </div>
+
+    <div class="container mt-4" id="footercopyright">
+      <div class="copyright">
+        <?php echo '&copy; ' . date('Y') . ' <strong><span>Impact</span></strong>. All Rights Reserved'; ?>
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/impact-bootstrap-business-website-template/ -->
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+
+  </footer>
