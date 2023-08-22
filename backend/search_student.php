2dda2c7bb8fbaa45740619beb26995998f592750
jonray <tacudog.jonray@gmail.com>
2023-08-22 13:09:52 +0800

    Remove codes

--- backend/search_student.php
+++ backend/search_student.php
@@ -1,36 +1,8 @@
 <?php
-
-// include '../backend/connect_database.php';
-// $sql = "SELECT `stud_user_id`, `first_name`, `last_name`, `gender`, `year_enrolled`, `course`, `birth_date`, `email` FROM student_user";
-
-// $result = $conn->query($sql);
-
-
-
-// $data = array();
-
-// if ($result->num_rows > 0) {
-//     while ($row = $result->fetch_assoc()) {
-//         $data[] = $row;
-//     }
-// }
-
-// $conn->close();
-
-// // Return data as JSON
-// echo json_encode($data);
-
-
-
-
-
 include '../backend/connect_database.php';
 $sql = "SELECT `stud_user_id`, `first_name`, `last_name`, `gender`, `year_enrolled`, `course`, `birth_date`, `email` FROM student_user";
 
 $result = $conn->query($sql);
-
-
-
 $data = array();
 
 if ($result->num_rows > 0) {
@@ -38,11 +10,9 @@ if ($result->num_rows > 0) {
         $data[] = $row;
     }
 }
-
-$conn->close();
-
 // Return data as JSON
 echo json_encode($data);
 
+$conn->close();
 
 ?>
