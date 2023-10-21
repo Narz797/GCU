3ce6d4fca29ff652a1c9bb278ea3f2bc2579dde6
Narz <101245624+Narz797@users.noreply.github.com>
2023-10-21 14:15:32 +0800

    db

--- database/db_gcu.sql
+++ database/db_gcu.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 20, 2023 at 07:50 AM
+-- Generation Time: Oct 21, 2023 at 08:15 AM
 -- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- PHP Version: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
