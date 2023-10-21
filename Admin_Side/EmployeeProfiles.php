98656b2eb1e922ab7b287270a839ed9dc5fe19bf
JulianaMae2002 <cianreviere@gmail.com>
2023-10-21 11:57:42 +0800

    edits

--- Admin_Side/EmployeeProfiles.php
+++ Admin_Side/EmployeeProfiles.php
@@ -4,9 +4,10 @@
 <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Employee Profiles</title>
+    <title>Employee Profile</title>
     <!-- Remix icons -->
     <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+    <link rel="icon" href="assets/images/GCU_logo.png">
     <!-- Vendor CSS Files -->
     <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
@@ -14,15 +15,11 @@
     <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
     <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
     <!-- Stylesheet -->
-    <link rel="stylesheet" href="assets/css/forms.css">
-
-    <link rel="icon" href="assets/images/GCU_logo.png">
-
-    <!-- jquery -->
+    <link rel="stylesheet" href="assets/css/profiles.css">
     <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
 
-      <!-- Export -->
-      <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
+    <!-- Export -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.7.1/css/buttons.dataTables.min.css">
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js"></script>
@@ -37,16 +34,18 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
     <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script>
     <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.print.min.js"></script>
-</head>
 
+</head>
+<style>
+
+    </style>
 <body>
 
-
-  <!-- Header -->
-  <header class="header">
+    <!-- Header -->
+    <header class="header">
     <nav class="nav"> 
         <div class="logo">
-            <a href="./main.php" ><img src="assets/images/bsu.png" alt=""></a>
+            <a href="./index.php" ><img src="assets/images/bsu.png" alt=""></a>
         </div>
         <div class="nav-mobile">
             <ul class="list">
@@ -54,7 +53,7 @@
                     <a href="main.php" class="list-link current">Home</a>
                 </li>
                 <li class="list-item hov">
-                    <a href="profile.php" class="list-link current1">Student Profiles</a>
+                    <a href="form.php" class="list-link current1">Requested Forms</a>
                 </li>
                 <li class="list-item hov">
                     <a href="appointment.php" class="list-link current1">Appointment Schedules</a>
@@ -79,126 +78,76 @@
     </nav>
 </header>
     <!-- Welcome-message -->
+    
 <section>
-<section class="banner">
-        <div class="banner-container">
-    <br>
-            <img src="assets/images/GCU_logo.png" alt="">
-            <div class="banner-text">
-                <h5>REPUBLIC OF THE PHILIPPINES</h5>
-                <hr class="banner-line">
-                <h2><span>BENGUET STATE UNIVERSITY</span></h2>
-                <h1>GUIDANCE AND COUNSELING UNIT</h1>
-            </div>
-        </div>
-    </section>
-    <div class="block"> 
+  <section class="banner">
+          <div class="banner-container">
+      <br>
+              <img src="assets/images/GCU_logo.png" alt="">
+              <div class="banner-text">
+                  <h5>REPUBLIC OF THE PHILIPPINES</h5>
+                  <hr class="banner-line">
+                  <h2><span>BENGUET STATE UNIVERSITY</span></h2>
+                  <h1>GUIDANCE AND COUNSELING UNIT</h1>
+              </div>
+          </div>
+      </section>
+      <div class="block"> 
     </div>
     <div class="title independent-title">
-        <h2>PENDING EMPLOYEE PROFILES</h2>
+    <h2>Employee Profiles</h2>
     </div>
-
+     <!-- Section -->
     <div class="container">
         <div class="card">
-           <header class="card-header">
-                <small>The following are the pending employee profiles for today,</small>
-                <h2 class="title">&nbsp&nbsp August 25, 2023</h2>
-            </header>
-            <hr>
-          <div class=" gallery">
-              <div id="dynamicContent"></div>
-
-        </div>
-        </div>
-    </div>
-
-    <!-- History of transaction -->
-    <div class="container">
-        <br>
-        <h2 class="title"></h2>
-        <div class="card">
-            <header class="card-header">
-                <h1>HISTORY</h1>
-                <p>&nbsp&nbsp The following are the previous requested forms.</p>
-            </header>
-            <hr>
-            <div class=" gallery">
-            <main class="table" id="customers_table">
-            <section class="table-header">
-                <h1>List of Requested Forms</h1>
-                <div class="input-group">
-                    <input type="search" placeholder="Search Data...">
-                </div>
-                <div class="export-file">
-                    <label for="export-file" class="export-file-btn" title="Export File"></label>
-                    <input type="checkbox" id="export-file">
-                    <div class="export-file-options">
-                        <p>Export as&nbsp; &#10140;</p>
-                        <label for="export-file" id="toPDF">PDF <img src="assets/images/pdf.png" alt=""></label>
-                        <label for="export-file" id="toEXCEL">EXCEL <img src="assets/images/excel.png" alt=""></label>
-                    </div>
-                </div>
-            </section>
-            <section class="table-body">
-                <table>
-                    <thead>
-                        <tr>
-                            <th> Id <span class="icon-arrow">&UpArrow;</span></th>
-                            <th> Student <span class="icon-arrow">&UpArrow;</span></th>
-                            <th> College <span class="icon-arrow">&UpArrow;</span></th>
-                            <th> Course <span class="icon-arrow">&UpArrow;</span></th>
-                            <th> Date <span class="icon-arrow">&UpArrow;</span></th>
-                            <th> Status <span class="icon-arrow">&UpArrow;</span></th>
-                            <th> Action <span class="icon-arrow">&UpArrow;</span></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td> 1 </td>
-                            <td> Zinzu Chan Lee</td>
-                            <td> CHET </td>
-                            <td> Psychology </td>
-                            <td> 17 Dec, 2022 </td>
-                            <td>
-                                <p class="status delivered">Delivered</p>
-                            </td>
-                            <td> 
-                                <i class="ri-delete-bin-6-line"></i>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td> 2 </td>
-                            <td> Jeet Saru </td>
-                            <td> CIS </td>
-                            <td> BSIT</td>
-                            <td> 27 Aug, 2023 </td>
-                            <td>
-                                <p class="status cancelled">Cancelled</p>
-                            </td>
-                            <td> 
-                                <i class="ri-delete-bin-6-line"></i>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td> 3 </td>
-                            <td> Sarita Limbu </td>
-                            <td> CAS </td>
-                            <td> Architecture </td>
-                            <td> 23 Apr, 2023 </td>
-                            <td>
-                                <p class="status pending">Pending</p>
-                            </td>
-                            <td> 
-                                <i class="ri-delete-bin-6-line"></i>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </section>
-            </main>
+        <div class="gallery">
+ <main class="table" id="customers_table">
+        <section class="table-header">
+            <h1>List of Employees</h1>
+            <div class="input-group">
+                <input type="text" id="searchInput" placeholder="Search Data...">
+                
             </div>
+            <div class="export-file">
+                <label for="export-file" class="export-file-btn" title="Export File"><img src="assets/images/export.png" alt=""></label>
+                <input type="checkbox" id="export-file">
+
+                <div class="export-file-options">
+                    <label>Export As &nbsp; &#10140;</label>
+                    <label for="export-file" id="toPDF">PDF <img src="assets/images/pdf.png" alt=""></label>
+                    <label for="export-file" id="toEXCEL">EXCEL <img src="assets/images/excel.png" alt=""></label>
+                </div>
+
+
+            </div>
+        </section>
+
+       
+
+        <section class="table-body" >
+            <table  id="table" >
+            <table>
+                <thead>
+                    <tr>
+                        <th> Employee ID # <br> <span class="icon-arrow">&UpArrow;</span></th>
+                        <th> Last Name <br><span class="icon-arrow">&UpArrow;</span></th>
+                        <th> First Name <br><span class="icon-arrow">&UpArrow;</span></th>
+                        <th> Contact Number <br><span class="icon-arrow">&UpArrow;</span></th>
+                        <th> Action </th>
+                    </tr>
+                </thead>
+                <tbody id="data-table">
+                  
+                 </tbody>
+             
+            </table>
+        </section>
+    </main>
+    </div>
         </div>
     </div>
+
+
 </section>
 <br>
 
@@ -211,70 +160,9 @@
         <div class="credits">Designed by <a href="https://www.facebook.com/">BSIT</a></div>
     </div>
 </footer>
-
 <!-- Script     -->
- 
-<script>
- $(document).ready(function() {
-    $.ajax({
-        url: "../backend/check_transaction.php",
-        type: "GET",
-        dataType: "json",
-        success: function(data) {
-          
-            var contentTemplate = '';
+<script src="./assets/main.js"></script>
 
-            for (var i = 0; i < data.length; i++) {
-                var entry = data[i];
-                var status = entry.status; // Store the status in a variable
-
-                contentTemplate += `
-                    <div class="content1" data-stud-user-id="${entry.stud_user_id}">
-                        <img src="./assets/images/${status === 0 ? 'a.jpg' : 'pfp.jpg'}">
-                        <h4>${entry.service_requested}</h4>
-                        <p>${entry.last_name}</p>
-                        <h5>${entry.stud_user_id}</h5>
-                        <br>
-                        <h5 class="status-icon">
-                            <i class="${status === 0 ? 'ri-mail-unread-line' : 'ri-mail-open-line'}"></i>
-                        </h5>
-                        <br>
-                        ${status === 0 ? '<a href="#">' : '<a href="#">'}
-                            <button class="buy-${status === 0 ? 1 : 2}" ${status === 1 ? 'disabled' : ''}>READ MORE</button>
-                        </a>
-                    </div>
-                `;
-            }
-
-            $("#dynamicContent").html(contentTemplate);
-
-            $(".content1 button").click(function() {
-    var contentElement = $(this).closest(".content1");
-    var studUserId = contentElement.data("stud-user-id"); // Extract stud_user_id from content1
-
-    // Make an AJAX request to update the status
-    $.ajax({
-        url: "../backend/update_status.php",
-        type: "POST",
-        data: { stud_user_id: studUserId },
-        success: function(response) {
-          console.log(response); // Log the response from the server
-    console.log("Status updated in the database");
-        },
-        error: function(xhr, status, error) {
-            console.error("Request failed with status: " + status);
-        }
-    });
-});
-
-        },
-        error: function(xhr, status, error) {
-            console.error("Request failed with status: " + status);
-        }
-    });
-});
-
-</script>  
-<script src="./assets/main.js"></script> 
+ <script src="assets/js/table.js"></script>   
 </body>
 </html>
--- Admin_Side/main.php
+++ Admin_Side/main.php
@@ -27,10 +27,10 @@ $id = $_SESSION['session_id'];
 
 <body>
     <!-- Header -->
-<header class="header">
+    <header class="header">
     <nav class="nav">
         <div class="logo">
-         <a href="./main.php" ><img src="assets/images/bsu.png" alt=""></a>
+         <a href="./index.php" ><img src="assets/images/bsu.png" alt=""></a>
         </div>
         <div class="align-right">
         <button class="icon-btn theme-toggle-btn place-items-center">
@@ -40,7 +40,7 @@ $id = $_SESSION['session_id'];
         </div>
     </nav>
 </header>
-    <!-- Welcome-message -->
+    <!-- Banner -->
 <section>
     <section class="banner">
         <div class="banner-container">
@@ -54,27 +54,35 @@ $id = $_SESSION['session_id'];
         </div>
         </div>
     </section>
-    <div class="block"> 
-    </div>
+    <div class="block"></div>
+    <!-- First Section -->
     <div class="title independent-title">
         <h2 > Control Panel</h2>
     </div>
     <div class="card">
         <header class="card-header">
             <small>Profile Account</small>
+
+<!-- call employee id 
+    number or 
+    profession = "Admin"-->
+
             <h2 class="title">Welcome back,&nbspAdmin</h2>
         </header>
         <hr>
         <div class="card-body">
             <div class="card-image">
-                <img src="./assets/images/sp.jpg" alt="">
+                <img src="assets/images/sp.jpg" alt="">
             </div>
             <div class="card-information">
+
+<!-- call employee 
+    registered data -->
                 <h1 class="title main-title"><span class="title-lastname main-title">uchiha,</span> Itachi Verlyn Rizz M.</h1>
-                <p class="card-description1">Joined at <span>January 05, 0000</span><br><br></p>
+                <p class="card-description1">Joined at <b id="date_joined"></b><br><br></p>
                 <p class="card-description">
-                    <span>Email:</span> BSU_Rizz123@gcu.com<br>
-                    <span>Position:</span> Grand Counselor
+                    <span>Email:</span><b id="employee_email"></b><br>
+                    <span>Position:</span><b id="employee_position"></b>
                 </p>
             </div>
             <div class="card-image1">
@@ -83,8 +91,8 @@ $id = $_SESSION['session_id'];
         </div>
     </div>
 </section>
-    <!-- Management-area -->
-<section class="management-area">
+ <!-- Management-area -->
+ <section class="management-area">
     <div class="management-area-container d-grid">
         <div class="card">
             <header class="card-header header-side">
@@ -105,10 +113,7 @@ $id = $_SESSION['session_id'];
                 <a href="appointment.php" class="card-body-link">
                 <i class="ri-calendar-line"></i>Appointment Schedules
                 </a>
-                <a href="statistics.php" class="card-body-link">
-                <i class="ri-folder-line"></i>Statistics
-                </a>
-                <a href="../index.php" class="card-body-link">
+                <a href="../index.php?logout=true" class="card-body-link">
                 <i class="ri-user-3-line"></i>Log-Out
                 </a>
             </div>
@@ -116,43 +121,75 @@ $id = $_SESSION['session_id'];
         <div class="card-group d-grid">
             <div class="card border one">
                 <div>
+
+<!-- call "b" contents from
+    data-->
                     <h2 class="title">LATEST Requested Forms</h2>
-                    <p class="card-description"><b>Sasuke Uchiha</b> has requested a <b>Readmission</b> form for the 57th times.</p>
-                </div>
-                <a href="index.php"><button class="list-link">Read More</button></a>
-            </div>
-            <div class="card border two">
-                <div>
-                    <h2 class="title">UNOPENED Requested Forms</h2>
-                    <p class="card-description"> <b>58</b> Forms waiting...</p>
+                    <p class="card-description2"><b id="studentId"></b> has requested a <b id="transactType"></b> form.</p>
                 </div>
                 <a href="form.php"><button class="list-link">Read More</button></a>
             </div>
+            <div class="card border two">
+                <div>
+                    <h2 class="title"><i>UNREAD</i> &nbspForms</h2>
+                    <div class="wrapper">
+                    <a href="form.php"><button class="list-link">Read More</button></a>
+                     <div class="count">
+                        <i class="ri-file-copy-2-line"></i>
+
+<!-- get sum of the 
+    target count = "data-val"-->
+
+                        <span class="num" data-val="315"><b id="total"></b></span>
+                        <span class="text">Forms waiting...</span>
+                    </div>
+                </div>
+                </div>
+            </div>
+            <!-- <div class="card border two">
+                <div>
+                    <h2 class="title">UNOPENED Requested Forms</h2>
+                    <p class="card-description"> <b id="total"></b> Forms waiting...</p>
+                </div>
+                <a href="form.php"><button class="list-link">Read More</button></a>
+            </div> -->
             <div class="card border three">
                 <div>
+                    <h2 class="title"><i>TODAY'S</i> &nbspAppointment</h2>
+                    <div class="wrapper">
+                    <a href="appointment.php"><button class="list-link">Read More</button></a>
+                     <div class="count">
+                        <i class="ri-calendar-todo-fill"></i>
+
+<!-- get sum of the 
+    target count = "data-val"-->
+
+                        <span class="num" data-val="10"><b id="totalAppointments"></b></span>
+                        <span class="text">pending...</span>
+                    </div>
+                </div>
+                </div>
+            </div>
+            <!-- <div class="card border three">
+                <div>
                     <h2 class="title">Number of Appointments TODAY</h2>
-                    <p class="card-description"> <b>3</b> Appointments pending...</p>
+                    <p class="card-description"> <b id="totalAppointments"></b> Appointments pending...</p>
                 </div>
                 <a href="appointment.php"><button class="list-link">Read More</button></a>
-            </div>
+            </div> -->
             <div class="card border four">
                 <div>
                     <h2 class="title">HISTORY TRANSACTIONS</h2>
-                    <p class="card-description"> 
-                        Sasuke Uchiha..........Duel Form<br>
-                        Akatsuki Kisame......Appointment<br>
-                        Sasuke Uchiha..........Duel Form<br>
-                        Sasuke Uchiha..........Duel Form<br>
-                        Fourth Hokage..........Feedback Form<br>
-                        Sasuke Uchiha..........Duel Form<br>
-                        Sasuke Uchiha..........Duel Form<br>
-                        Akatsuki Kisame......Appointment<br>
-                        Akatsuki Pain.............Appointment<br>
-                        Sasuke Uchiha..........Duel Form<br>
-                        Sasuke Uchiha..........Duel Form<br>
-                        Akatsuki Deidara.....Apology Form<br>
-                        Sasuke Uchiha..........Duel Form
-                    </p>
+
+<!-- honestly just copy paste 
+    the database so no need 
+    for it's design, just call
+    the student name and the 
+    action the employee had 
+    taken-->
+                    <p class="card-description" id="list-history"> 
+                            <!-- Data will be inserted here dynamically -->
+                        </p>
                 </div>
             </div>
         </div>
@@ -168,6 +205,103 @@ $id = $_SESSION['session_id'];
     </div>
 </footer>
     <!-- Script -->
-<script src="./assets/index.js"></script>    
+<script src="./assets/index.js"></script>   
+<script src="./assets/js/count.js"></script>     
+<script>
+    // Function to update the HTML elements
+    function updateValues(studentId, transactType, total, totalAppointments, employee_email, employee_position, employee_date_joined) {
+        $('#studentId').text(studentId);
+        $('#transactType').text(transactType);
+        $('#total').text(total);
+        $('#employee_email').text(employee_email);
+        $('#employee_position').text(employee_position);
+        $('#date_joined').text(employee_date_joined);
+        $('#totalAppointments').text(totalAppointments);
+   
+
+    }
+
+    // Function to fetch data from get_transaction.php
+    function fetchData() {
+        console.log('AJAX request started');
+        $.ajax({
+            type: 'GET',
+            url: '../backend/get_transaction.php',
+            dataType: 'json',
+            
+                // ...
+                success: function (data) {
+                    console.log(data);
+                    if (data.latest_data.length > 0) {
+                            var studentId = data.latest_data[0].student_id;
+                            var transactType = data.latest_data[0].transact_type;
+                            var total = data.total_pending_transactions;
+                            var totalAppointments = data.total_appointments; // Define total here
+                            var employee_email = data.adminUserData[0].email;
+                            var employee_position = data.adminUserData[0].position;
+                            var employee_date_joined = data.adminUserData[0].date_joined;
+
+                            console.log(totalAppointments);
+
+                            updateValues(studentId, transactType, total, totalAppointments, employee_email, employee_position, employee_date_joined);
+                            console.log(total);
+
+                            // Start both counting animations
+                            countAppointments(totalAppointments);
+                            countForms(total);
+                        } else {
+                        // Handle the case when no results are found
+                        // You can update the UI as needed
+                        console.log('No results found');
+                    }
+
+            },
+            error: function (xhr, status, error) {
+                console.error('Error: ' + error);
+                console.error('Status: ' + status);
+                console.error('Response: ' + xhr.responseText);
+            }
+        });
+    }
+// Function to update the card description with data
+function updateCardDescription(data) {
+    const cardDescription = $('#list-history');
+    cardDescription.empty(); // Clear existing content
+
+    if (data.length > 0) {
+        data.forEach(item => {
+            cardDescription.append(`${item.student_id}..........${item.transact_type}<br>`);
+        });
+    } else {
+        cardDescription.text('No transactions with status "done" found.');
+    }
+}
+
+// Function to fetch data from get_transaction.php
+function HistoryData() {
+    console.log('AJAX request started');
+    $.ajax({
+        type: 'GET',
+        url: '../backend/get_done_transaction.php',
+        dataType: 'json',
+        // ...
+        success: function (data) {
+            console.log(data);
+            updateCardDescription(data);
+        },
+        error: function (xhr, status, error) {
+            console.error('Error: ' + error);
+            console.error('Status: ' + status);
+            console.error('Response: ' + xhr.responseText);
+        }
+    });
+}
+
+// Call the fetchData function when the page loads
+HistoryData();
+
+    // Call the fetchData function when the page loads
+    fetchData();
+</script> 
 </body>
 </html>
