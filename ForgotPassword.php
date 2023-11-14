8e6f4b38cada6100f63c48450161b5dffdadd769
Narz <101245624+Narz797@users.noreply.github.com>
2023-11-14 20:57:57 +0800

    on going forgo password

--- ForgotPassword.php
+++ ForgotPassword.php
@@ -10,7 +10,7 @@
       <img src="assets/img/GCU_logo.png" alt="Logo" width="90" height="90">
     </div>
     <h2  style="color:black; font-family: 'Lucida Console', Courier, monospace;">FORGOT PASSWORD</h2>
-    <form action="reset_password.php" method="post">
+    <form action="backend/password-reset-code.php" method="post">
       <label for="email" style="color:black;">Email*</label>
       <input type="email" id="email" name="email" required>
       
