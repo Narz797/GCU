7427dc372daa9731339f2a9536004adc5e7fd41d
neMz-123 <143060243+neMz-123@users.noreply.github.com>
2023-12-02 23:50:21 +0800

    design

--- includes/banner.php
+++ includes/banner.php
@@ -1,11 +1,17 @@
 <div style="background-image: url('assets/img/bg.png'); background-repeat: no-repeat; background-size: cover; display:flex; justify-content:center; padding-top:2%; padding-bottom:2%;">
   <br>
-  <img src="assets/img/GCU_logo.png" alt="" style="display: inline-block; vertical-align: middle;  width:10%; height:auto;">
-  <div id="RBG" style="display: inline-block; vertical-align: middle;">
+  <img src="assets/img/bsu.png" class="bsu" alt="" style="display: inline-block; vertical-align: middle;  margin-right:3%; width:10%; height:auto;">
+
+  <!-- <img src="assets/img/GCU_logo.png" alt="" style="display: inline-block; vertical-align: middle;  width:10%; height:auto;"> -->
+  <div id="RBG" style="display: inline-block; vertical-align: middle; margin-top:2%;">
     <h5 style="font-family: 'Georgia', serif; color:black;">REPUBLIC OF THE PHILIPPINES</h5>
     <hr class="line" style="width: 100%; border-color: black; margin-bottom: 0;">
-    <h1 style="font-family: 'Times New Roman', serif; color:black;"><span>BENGUET STATE UNIVERSITY</span></h1>
+    <!-- <h1 style="font-family: 'Times New Roman', serif; color:black;"><span>BENGUET STATE UNIVERSITY</span></h1> -->
+    <h1 style="font-family: fantasy; color:black;"><span>BENGUET STATE UNIVERSITY</span></h1>
+
     <h1 style="font-family: 'Garamond', serif; font-weight: bold; color:black;">GUIDANCE AND COUNSELING UNIT</h1>
+    <!-- <h1 style="font-family: fantasy; font-weight: bold; color:black;">GUIDANCE AND COUNSELING UNIT</h1> -->
+
   </div>
 </div>
 
--- index.php
+++ index.php
@@ -21,6 +21,7 @@ if ((isset($_GET['logout']) && $_GET['logout'] === 'true') || (isset($_SESSION) 
 <!-- test -->
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="UTF-8">
 </head>
 <style>
   @import url('https://fonts.googleapis.com/css?family=Roboto');
@@ -64,51 +65,64 @@ if ((isset($_GET['logout']) && $_GET['logout'] === 'true') || (isset($_SESSION) 
     .right-column {
       margin-top: 5%;
     }
-
+    
+   
     /* Add other responsive styles as needed */
   }
 </style>
 <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
 <body>
   <!-- ======= Hero Section ======= -->
-  <section id="hero" class="hero">
-    <div class="container position-relative">
+  <!-- <section id="hero" class="hero"  style="background: linear-gradient(to right,#2b9348,#f7b801,#55a630  );"> -->
+  <!-- <section id="hero" class="hero"  > -->
+      <!-- <block style="background:#aacc00; height:20px; " ></block> -->
+      <!-- <hr style="background:#aacc00; height:20%; "></hr> -->
+
+      <section id="hero" class="hero"  >
+
+
+
+    <div class="container position-relative" >
       <div class="row gy-5" data-aos="fade-in">
         <div class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center text-center text-lg-start">
-          <h1>Welcome to <span>BENGUET STATE UNIVERSITY</span></h1>
-          <h2>GUIDANCE AND COUNSELING UNIT</h2>
+          <h2 style="font-family: 'Lucida Calligraphy'; color:black;">Welcome to <br> <span style="font-family:fantasy;">BENGUET STATE UNIVERSITY</span></h2>
+          <h2  style="font-family: 'Lucida Calligraphy'; color:#f7b801;">Guidance and Counselling Unit</h2>
         </div>
         <div class="col-lg-6 order-1 order-lg-2">
-          <img src="assets/img/guidance.png" class="img-fluid" alt="" data-aos="zoom-out" data-aos-delay="100"
+          <img src="assets/img/aaa.png" class="img-fluid" alt="" data-aos="zoom-out" data-aos-delay="100"
             style="width: 3000px; height: auto;">
         </div>
       </div>
     </div>
+    
+    
+    
+  
     <div class="icon-boxes position-relative">
       <div class="container position-relative">
-        <div class="row gy-4 mt-5">
+        <div class="row gy-4 mt-5"  >
           <div class="col-xl-3 col-md-6" data-aos="fade-up" data-aos-delay="200">
-            <div class="icon-box" >
-              <div class="icon"><i class="bi bi-flag"></i></div>
-              <h4 class="title"><a href="#hs" class="stretched-link">OUR PRINCIPLE</a></h4>
+            <div class="icon-box"style="background: linear-gradient(to right,#bfd200, #80b918, #2b9348);" >
+              <div class="icon" style="color:black;" ><i class="bi bi-flag"></i></div>
+              <h4 class="title"><a href="#hs" class="stretched-link" style="color:black;" >OUR PRINCIPLE</a></h4>
             </div>
           </div><!--End Icon Box -->
           <div class="col-xl-3 col-md-6" data-aos="fade-up" data-aos-delay="300">
-            <div class="icon-box"  >
-              <div class="icon"><i class="bi bi-eye"></i></div>
-              <h4 class="title"><a href="#hs" class="stretched-link">OUR MANDATE</a></h4>
+            <div class="icon-box" style="background: linear-gradient(to right,#bfd200, #80b918, #2b9348);" >
+              <div class="icon" style="color:black;" ><i class="bi bi-eye"></i></div>
+              <h4 class="title"><a href="#hs" class="stretched-link" style="color:black;" >OUR MANDATE</a></h4>
             </div>
           </div><!--End Icon Box -->
           <div class="col-xl-3 col-md-6" data-aos="fade-up" data-aos-delay="500">
-            <div class="icon-box" style="  font-family: var(--font-primary)  ">
-              <div class="icon"><i class="bi bi-tools"></i></div>
-              <h4 class="title"><a href="#hs" class="stretched-link">OUR AIM</a></h4>
+            <div class="icon-box" style="  font-family: var(--font-primary)  ; background: linear-gradient(to right,#bfd200, #80b918, #2b9348);">
+              <div class="icon" style="color:black;"  ><i class="bi bi-tools"></i></div>
+              <h4 class="title"><a href="#hs" class="stretched-link" style="color:black;" >OUR AIM</a></h4>
             </div>
           </div><!--End Icon Box -->
           <div class="col-xl-3 col-md-6" data-aos="fade-up" data-aos-delay="100">
-            <div class="icon-box" style=" background: cream">
-              <div class="icon"><i class="bi bi-clock-history"></i></div>
-              <h4 class="title"><a href="https://www.facebook.com/profile.php?id=61553066382035" class="stretched-link">CONTACT</a></h4>
+            <div class="icon-box"style="background: linear-gradient(to right,#bfd200, #80b918, #2b9348); ">
+              <div class="icon" style="color:black;" ><i class="bi bi-clock-history"></i></div>
+              <h4 class="title" ><a href="https://www.facebook.com/profile.php?id=61553066382035" style="color:black;"  class="stretched-link" >CONTACT</a></h4>
             </div>
           </div><!--End Icon Box -->
         </div>
@@ -133,20 +147,20 @@ if ((isset($_GET['logout']) && $_GET['logout'] === 'true') || (isset($_SESSION) 
         <div class="row gy-4" data-aos="fade-up" data-aos-delay="100">
           <div class="col-lg-4 col-md-6">
             <div class="service-item  position-relative"
-              style=" background: -webkit-linear-gradient(left, #eee6e6,#FAFAD2);">
+              style=" background: linear-gradient(to right,#ede0d4,#ffc971  );">
               <div class="icon">
                 <i class="bi bi-people"></i>
               </div>
               <h3>Our Principle</h3>
               <!-- <p style="font-size:16px;"><b>TUTUKK: "Kalinga"</b></p> -->
-              <p style="font-size:16px;  font-family: var(--font-primary) ">A service-oriented partner for the
+              <p style="font-size:16px;  font-family: var(--font-primary) " >A service-oriented partner for the
                 development of competitive and empowered students.
               </p>
             </div>
           </div><!-- End Service Item -->
           <div class="col-lg-4 col-md-6">
             <div class="service-item position-relative"
-              style=" background: -webkit-linear-gradient(left, #eee6e6, 	#FAFAD2);">
+            style=" background: linear-gradient(to right,#ede0d4,#ffc971  );">
               <div class="icon">
                 <i class="bi bi-broadcast"></i>
               </div>
@@ -159,7 +173,7 @@ if ((isset($_GET['logout']) && $_GET['logout'] === 'true') || (isset($_SESSION) 
           </div><!-- End Service Item -->
           <div class="col-lg-4 col-md-6">
             <div class="service-item position-relative"
-              style=" background: -webkit-linear-gradient(left, #eee6e6, 	#FAFAD2);">
+            style=" background: linear-gradient(to right,#ede0d4,#ffc971  );">
               <div class="icon">
                 <i class="bi bi-easel"></i>
               </div>
