e745e8114661feeb48b71e242eb6558567a0f2c5
Narz <101245624+Narz797@users.noreply.github.com>
2023-12-14 00:57:42 +0800

    "refining"

--- Student_Side/student-profile2.php
+++ Student_Side/student-profile2.php
@@ -127,7 +127,7 @@ $siblings = $siblings->fetchAll();
 
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link href="../assets/img/GCU_logo.png" rel="icon">
-  <link rel="stylesheet" href="../assets/contact.css">
+  <link rel="stylesheet" href="../assets/css/contact.css">
   <link rel="stylesheet" href="assets/stud_prof.css">
 
       <!-- Fontawesome CDN Link -->
